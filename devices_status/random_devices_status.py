f8b2796011843a0ad3881e0e5b42a81cbf4fd75d
Mongar28 <cristian.montoya.g@gmail.com>
2023-12-15 13:55:09 -0500

    actualizacion - devices_status

--- devices_status/random_devices_status.py
+++ devices_status/random_devices_status.py
@@ -1,49 +1,39 @@
 import random
+import json
 from datetime import datetime
 
 
-#Possible states of devices.
-status: list = ['excellent', 'good', 'warning', 'faulty', 'killed', 'unknown']
-devices: list = ['satelite', 'spacecraft', 'space vehicle', 'Trajes','other components']
-missions: list = ['OrbitOne', 'ColonyMoon', 'VacMars', 'GalaxyTwo', 'Unknow']
 
+def random_device_statu(missions: list, devices: list):
 
-
-def mission_and_device_selector(missions: list):
-
+    status: list = ['excellent', 'good', 'warning', 'faulty', 'killed', 'unknown'] 
     range_selecting = random.randint(1, 5)
     selected_mission: list = random.sample(missions, range_selecting)
     print(selected_mission)
-    
-    def random_device_status(selected_mission):
-        list_data_divises: list = ['satelite', 'spacecraft', 'space vehicle', 'Trajes','other components']
-        data_divises: dict = {}
-        status: list = ['excellent', 'good', 'warning', 'faulty', 'killed', 'unknown']
-        selected_status: str = random.choice(status)
-        date_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        lista = []
-        
-        for mission in selected_mission:
-            for device in list_data_divises:
-                data_divises[mission] = {
-                'date_time': date_time,
-                'type_divese': device,
-                'status_devise': selected_status
-                }
 
-                lista.append(data_divises)
-                data_divises: dict = {}
-        return lista
-    return random_device_status(selected_mission)
+    dices_status: dict = {}
+    fecha = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
 
-        
+    for mission in selected_mission:
+        dices_status[mission] = {}
+        for device in devices:
+            dices_status[mission]['fecha'] = fecha
+            dices_status[mission][device] = random.choice(status)
             
+            
+    return dices_status
+        
 
+
+#Possible states of devices.
+
+devices: list = ['satelite', 'spacecraft', 'space vehicle', 'spacesuit','other components']
+missions: list = ['OrbitOne', 'ColonyMoon', 'VacMars', 'GalaxyTwo', 'Unknow']
     
 
+# Imprimir diccionario en formato JSON con indentación
 
-datos = mission_and_device_selector(missions)
+dices_status = random_device_statu(missions, devices)
+json_string = json.dumps(dices_status, indent=4)
+print(json_string)
 
-print(datos)
-    
-    
