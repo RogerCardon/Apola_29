07fc0f568e3aa00fb307056fc1bdff012f56e423
Mongar28 <cristian.montoya.g@gmail.com>
2024-01-05 13:58:54 -0500

    Actualización

--- Apolo-11.py
+++ Apolo-11.py
@@ -1,24 +1,19 @@
 # importación de los modulos a utilizar
-
 from devices_status.random_devices_status import random_device_status
 import json
 import yaml
-
+from tools.load_tools import configuration_file_load
+from tools.write_tools import consecutive_number_update
 
 
 # Cargar configuraciones desde el archivo YAML
-with open('config.yml', 'r') as file:
-    config_data = yaml.safe_load(file)
-
-    
-# Crear las variables que contienen los datos de configuracion 
-missions: list =  config_data['list_misions']
-devices: list =  config_data['list_divices']
-status: list =  config_data['devices_status']
-data_mission_devices:dict= random_device_status(missions, devices, status)
+configuration_data: dict = configuration_file_load()
 
 # Ejecutar las funcion random_device_status ingresando los datos de configuracion previamente cargados 
-dices_status = random_device_status(missions, devices, status)
+data_mission_devices:dict= random_device_status(configuration_data['missions'],
+                                                configuration_data['devices'],
+                                                configuration_data['status'])
+
 
 
 # Imprimimos el diccionario que retorna la funcion random_device_status
--- tools/load_tools.py
+++ tools/load_tools.py
@@ -1,17 +1,24 @@
+"""Esta librería permite manipular archivos .yml"""
 import yaml
 
 
-def configuration_file_load():
+def configuration_file_load()->None:
+    """_summary_
+
+    Returns:
+        _type_: _description_
+    """
+
     # load configurations from YAML file
-    with open('config.yml', 'r') as file:
+    with open('config.yml', 'r', encoding="utf-8") as file:
         config_data = yaml.safe_load(file)
-        
+
     # Create the variables that will contain the configuration data
     missions = config_data['missions']
     devices = config_data['devices']
     status = config_data['divices_statug']
-    consecutive_number = config_data['consecutive_number'] 
-    
+    consecutive_number = config_data['consecutive_number']
+
     return {
         'missions': missions,
         'devices': devices,
